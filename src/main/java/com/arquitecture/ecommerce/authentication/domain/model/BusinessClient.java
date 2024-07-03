04759df002324e9092a84505708a6657783904a5
Angye123an <58081483+Angye123an@users.noreply.github.com>
2024-07-02 21:10:40 -0500

    Add files via upload

---
+++ src/main/java/com/arquitecture/ecommerce/authentication/domain/model/BusinessClient.java
@@ -0,0 +1,22 @@
+package com.arquitecture.ecommerce.authentication.domain.model;
+
+public class BusinessClient extends Client{
+    private String ruc;
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+}
